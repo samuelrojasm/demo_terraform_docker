5cd28845e3083674c11a38691a0c8dfd05e93e0c
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-27 12:40:35 -0600

    demo_tf_docker

---
+++ labs_iniciales/lab_build_image/main.tf
@@ -0,0 +1,14 @@
+# ==================================================================================
+# Objetivo: 
+#           Crear una imagen Docker simple a partir de una imagen base (nginx).
+#           Crear Terraform Infrastructure con Docker.
+# Verificar: 
+#           Al terminar terraform apply:
+#               dokcer image ls
+# ==================================================================================
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
---
+++ labs_iniciales/lab_run_container/main.tf
@@ -0,0 +1,27 @@
+# ==================================================================================
+# Objetivo: 
+#           CEjecutar un contenedor Docker desde una imagen oficial.
+#           Crear Terraform Infrastructure con Docker.
+#           Learn the Terraform workflow by deploying an NGINX Docker container.
+# Verificar: 
+#           Al terminar terraform apply:
+#               dokcer image ls
+#               docker container ls
+#               acceder a: http://localhost:8080
+#
+# ==================================================================================
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx_server" {
+  image = docker_image.nginx.image_id
+  name  = "lab_nginx_server"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
