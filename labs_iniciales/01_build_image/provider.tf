ad17cc99dcd6a03531b9a286654e9e5ad667543f
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-06-27 19:42:56 -0600

    demo_tf_docker

---
+++ labs_iniciales/01_build_image/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "~> 1.12"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6"
+    }
+  }
+}
+
+provider "docker" {}
